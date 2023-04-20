33e9e224476b41954bc44e91aa590403adeb2ca2
Ryan Lee <ryanbylee@ucla.edu>
2023-04-19 19:17:20 -0700

    implemented custom dataset class using torch.utils.data.datset

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -1,19 +1,35 @@
 import torch
+import os
+import pandas as pd
+from torchvision.io import read_image
+
 
 
 class StartingDataset(torch.utils.data.Dataset):
     """
-    Dataset that contains 100000 3x224x224 black images (all zeros).
+    Starting dataset contains 100000 3x224x224 black images (all zeros).
+    Below is modified to take custom dataset.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, mapping_file, img_directory, transform = None, target_transform = None):
+        self.labels = pd.read_csv(mapping_file)
+        self.img_directory = img_directory
+        self.transform = transform
+        self.target_transform = target_transform
+        
 
     def __getitem__(self, index):
-        inputs = torch.zeros([3, 224, 224])
-        label = 0
+        path = os.path.join(self.img_directory, self.labels.iloc[index, 0])
+        image = read_image(path)
+        label = self.labels.iloc[index, 1]
 
-        return inputs, label
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        return image, label
 
     def __len__(self):
-        return 10000
+        return len(self.labels)
+    
