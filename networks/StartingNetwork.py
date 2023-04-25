8758de2cf3111dba660b5af3e1b53d6d8fca6619
Ryan Lee <ryanbylee@ucla.edu>
2023-04-24 18:56:09 -0700

    implemented basic network structure

--- networks/StartingNetwork.py
+++ networks/StartingNetwork.py
@@ -7,11 +7,26 @@ class StartingNetwork(torch.nn.Module):
     Basic logistic regression on 224x224x3 images.
     """
 
+
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.fc = nn.Linear(224 * 224 * 3, 1)
-        self.sigmoid = nn.Sigmoid()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=3,
+                out_channels=4,
+                kernel_size=5,
+                
+            )
+
+
+
+        )
+
+
+
+        # self.flatten = nn.Flatten()
+        # self.fc = nn.Linear(224 * 224 * 3, 1)
+        # self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.flatten(x)
